d20f39e03ddd0d32cc4e33fa12727638d0b4fe4f
Zhe YAN <155991918+Zhe-YAN-Berlin@users.noreply.github.com>
2024-02-15 16:15:40 +0000

    update

--- 1_docker_sql/pipeline.py
+++ 1_docker_sql/pipeline.py
@@ -1,3 +1,6 @@
 import pandas as pd 
+import sys
+print(sys.argv)
+day = sys.argv[1]
 # some fancy stuff with pandas
-print ('job finished successfully')
+print ('job finished successfully for day = f{day}')
