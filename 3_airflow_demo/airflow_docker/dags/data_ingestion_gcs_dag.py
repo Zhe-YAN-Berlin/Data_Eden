77d434cf25efc4945e1e45564c639cfeebc7be7b
ZHEfromLocal <zhe.yan.cn@gmail.com>
2024-02-21 15:00:28 +0000

    update

--- 3_airflow_demo/airflow_docker/dags/data_ingestion_gcs_dag.py
+++ 3_airflow_demo/airflow_docker/dags/data_ingestion_gcs_dag.py
@@ -1,32 +1,22 @@
 import os
 import logging
+from datetime import timedelta
+import pendulum
 
 from airflow import DAG
-from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
-path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
-
-
-def format_to_parquet(src_file):
-    if not src_file.endswith('.csv'):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+parquet_file = "yellow_tripdata_2023-01.parquet"
+parquet_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{parquet_file}"
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/home/datatalks_jan/Data_Eden/3_airflow_demo/airflow_docker")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all') 
 
 
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
@@ -53,7 +43,7 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
+    "start_date": pendulum.today("UTC").subtract(days=1),
     "depends_on_past": False,
     "retries": 1,
 }
@@ -61,24 +51,16 @@ default_args = {
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
     dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
+    schedule=timedelta(days=1),
     default_args=default_args,
     catchup=False,
     max_active_runs=1,
-    tags=['dtc-de'],
+    tags=['zhe-data-2-gcs'],
 ) as dag:
 
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
-    )
-
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-        },
+    download_parquet_task = BashOperator(
+        task_id="download_parquet_task",
+        bash_command=f"curl -sSL {parquet_url} > {path_to_local_home}/{parquet_file}"
     )
 
     # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
@@ -87,7 +69,7 @@ with DAG(
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
+            "object_name": f"{parquet_file}",
             "local_file": f"{path_to_local_home}/{parquet_file}",
         },
     )
@@ -102,9 +84,9 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/{parquet_file}"],
             },
         },
     )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
