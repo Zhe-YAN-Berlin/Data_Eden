835ba7a017402b13ce877703e9e851bc4cfd4442
Zhe <datatalks.jan@gmail.com>
2024-04-04 14:05:18 +0000

    UPDATE

--- 3_airflow_demo/airflow/dags/dag_spark_multi_source_test_docker.py
+++ 3_airflow_demo/airflow/dags/dag_spark_multi_source_test_docker.py
@@ -1,19 +1,33 @@
 import gdown
 import os
 from airflow import DAG
-from airflow.operators.docker_operator import DockerOperator
+from airflow.providers.docker.operators.docker import DockerOperator, Mount
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from google.cloud import storage
 
 #remember pip install apache-airflow-providers-docker
 credentials_location = '/home/datatalks_jan/.google/credentials/google_credentials.json'
 
+def download_and_unzip_data():
+    source_url = 'https://drive.google.com/uc?id=1vcb_HBWsOSKW4XxhLfRpGlLzBLwHlGWJ'
+    output_path = '/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/multi_source_data'
+    output_file = os.path.join(output_path, 'multi_source_demo.zip') 
+    gdown.download(source_url, output_file, quiet=False)
+    os.system(f'unzip -o {output_file} -d {output_path}')
+
 def data_outbound_GCS(ti):
-    df_final = ti.xcom_pull(task_ids='task_2_spark')
-    #df_final.write.csv("gs://my-zhe-414813/conrad_output", mode="overwrite", header=True, inferSchema=True)
-    output_path = '/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/output/df_final.txt'
-    df_final.write.text(output_path, mode="overwrite")
+    df_str = ti.xcom_pull(task_ids='task_2_spark')
+    with open("/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/output/file.txt", "w") as file:
+        file.write(df_str[0])
+    local_file_path = '/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/output/file.txt'
+    gcs_bucket_name = 'my-zhe-414813'
+    gcs_file_path = 'conrad_output/file.txt'
+    client = storage.Client()
+    bucket = client.get_bucket(gcs_bucket_name)
+    blob = bucket.blob(gcs_file_path)
+    blob.upload_from_filename(local_file_path)
 
 default_args = {
     'owner': 'airflow', 
@@ -27,15 +41,19 @@ default_args = {
 
 with DAG('conrad_coding_test', default_args=default_args, schedule_interval='@weekly') as dag:
 
+    task_1_ingestion = PythonOperator(
+        task_id='task_1_ingestion',
+        python_callable=download_and_unzip_data,
+        dag=dag
+    )
+
     task_2_spark = DockerOperator(
         task_id='task_2_spark',
         image = 'sparkhome:latest',
         command='python /opt/spark/etl_task_2.py',
         docker_url='unix://var/run/docker.sock',
         xcom_all = True,
-        mounts = [
-            Mount(source='/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/multi_source_data', target='/opt/spark/mount_data')
-        ],
+        mounts=[Mount(source='/home/datatalks_jan/Data_Eden/8_pySpark_pilot/source_data/multi_source_data',target='/opt/spark/mount_data',type='bind')],
         dag=dag
     )
 
@@ -45,4 +63,4 @@ with DAG('conrad_coding_test', default_args=default_args, schedule_interval='@we
         provide_context=True,
         dag=dag
     )
-task_2_spark >> task_3_data_output
+task_1_ingestion >> task_2_spark >> task_3_data_output
