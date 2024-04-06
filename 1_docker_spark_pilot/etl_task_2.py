b58caab18ce6d3c26e6ab86af118912bdf1fa7b6
Zhe <datatalks.jan@gmail.com>
2024-04-06 21:03:20 +0000

    update

--- 1_docker_spark_pilot/etl_task_2.py
+++ 1_docker_spark_pilot/etl_task_2.py
@@ -8,19 +8,18 @@ from pyspark.sql.functions import col, acos, cos, sin, radians, atan2, sqrt
 from pyspark.sql.window import Window
 from datetime import datetime
 
+### spark configuration ###
 conf = SparkConf() \
     .setMaster('local[*]') \
     .setAppName('conrad_test')
 
 sc = SparkContext(conf=conf)
 
-test = []
-test0 = []
-
 spark = SparkSession.builder \
     .config(conf=sc.getConf()) \
     .getOrCreate()
 
+### DataFrame 1 ###
 df_1_users = spark.read.option("header", "true").csv('/opt/spark/mount_data/users.csv')
 df_1_distri_centers = spark.read.option("header", "true").csv('/opt/spark/mount_data/distribution_centers.csv')
 
@@ -28,15 +27,6 @@ df_1_users = df_1_users.withColumn("latitude", df_1_users["latitude"].cast("floa
 df_1_users = df_1_users.withColumn("longitude", df_1_users["longitude"].cast("float"))
 df_1_distri_centers = df_1_distri_centers.withColumn("latitude", df_1_distri_centers["latitude"].cast("float"))
 df_1_distri_centers = df_1_distri_centers.withColumn("longitude", df_1_distri_centers["longitude"].cast("float"))
-
-def haversine_distance(lat1, lon1, lat2, lon2):
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = 6371 * c 
-    return distance
 
 df_1_users.createOrReplaceTempView("users")
 df_1_distri_centers.createOrReplaceTempView("distribution_centers")
@@ -48,15 +38,25 @@ df_1_sql_query = """
     FROM users u
     CROSS JOIN distribution_centers dc
 """
+# define haversine function for calculating distiance between two GEO points
+def haversine_distance(lat1, lon1, lat2, lon2):
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = 6371 * c 
+    return distance
 
 df_1_with_distance = spark.sql(df_1_sql_query).withColumn("distance", haversine_distance(col("user_latitude"), col("user_longitude"), col("center_latitude"), col("center_longitude")))
 
+# window function for getting nearst distribution station
 windowSpec = Window.partitionBy("user_id").orderBy("distance")
 df_with_row_number = df_1_with_distance.withColumn("row_number", row_number().over(windowSpec))
 df_1_final = df_with_row_number.filter(col("row_number") == 1)
 df_1 = df_1_final.select("user_id","user_age","country","state","distribution_center_id","distribution_center_name","distance").orderBy("user_id")
 
-
+### DataFrame 2 ###
 df_2_orders = spark.read.option("header", "true").csv('/opt/spark/mount_data//orders.csv')
 df_2_order_items = spark.read.option("header", "true").csv('/opt/spark/mount_data//order_items.csv')
 
@@ -78,6 +78,7 @@ df_2 = spark.sql("""
     GROUP BY user_id
 """)
 
+### DataFrame 3 ###
 df_3_product_price = spark.read.option("header", "true").csv('/opt/spark/mount_data/products.csv')
 df_3_product_price = df_3_product_price.select("id", "cost")
 df_3_product_price.createOrReplaceTempView("product_price")
@@ -96,6 +97,7 @@ when(df['profit_total'] < 50, '1')
 .when(df['profit_total'] > 150, '3')
 )
 
+### combine DataFrame 1&2&3 ###
 df_final = df_1.join(df_2, "user_id").join(df_3, "user_id")
 df_final = df_final.select("user_id","user_age","country","state","distribution_center_id","distribution_center_name","product_return_rate","profit_level")
 df_final = df_final.withColumn("user_age", df_final["user_age"].cast("int"))
